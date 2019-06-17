8a8d0c64ae87499c36b6dc00d058f060bbd88ec6
JoshuaMbiyu <joshuambiyu02@gmail.com>
2019-06-17 11:28:21 +0300

    bankcreation outer closure method making and usage

---
+++ bank-example-closure.ts
@@ -0,0 +1,23 @@
+function createBankAccount<T extends number>(owner: string, initialBalance: T) {
+    let balance = initialBalance; //private variable
+
+    return { //factory function
+        deposit(amount: T) {
+            balance = (balance + (amount as any)) as T;
+            console.log(`${owner} deposited ${amount}. Balance: ${balance}`);
+        },
+        withdraw(amount: T) {
+            if (amount > balance) {
+                console.log("Insufficient funds");
+                return;
+            }
+            balance = (balance - (amount as any)) as T;
+            console.log(`${owner} withdrew ${amount}. Balance: ${balance}`);
+        },
+        getBalance() {
+            return balance;
+        }
+    };
+}
+// account creation usage
+const bankUsage = createBankAccount("Joshua", 100)
