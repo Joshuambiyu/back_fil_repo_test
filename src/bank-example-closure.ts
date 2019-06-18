423bf9cbaa3258bab38f3456a8bc965df53d8c6d
JoshuaMbiyu <joshuambiyu02@gmail.com>
2019-06-18 11:15:02 +0300

    bank deposit and check ballance with this keyword for chaining

--- src/bank-example-closure.ts
+++ src/bank-example-closure.ts
@@ -5,6 +5,7 @@ function createBankAccount<T extends number>(owner: string, initialBalance: T) {
         deposit(amount: T) {
             balance = (balance + (amount as any)) as T;
             console.log(`${owner} deposited ${amount}. Balance: ${balance}`);
+            return this // to enable chaining
         },
         withdraw(amount: T) {
             if (amount > balance) {
@@ -13,6 +14,7 @@ function createBankAccount<T extends number>(owner: string, initialBalance: T) {
             }
             balance = (balance - (amount as any)) as T;
             console.log(`${owner} withdrew ${amount}. Balance: ${balance}`);
+            return this //to enable chaining
         },
         getBalance() {
             return balance;
@@ -21,3 +23,4 @@ function createBankAccount<T extends number>(owner: string, initialBalance: T) {
 }
 // account creation usage
 const bankUsage = createBankAccount("Joshua", 100)
+console.log(bankUsage.deposit(100).getBalance())
